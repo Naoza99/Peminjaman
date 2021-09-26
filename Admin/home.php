8f62b80dffb5b0016e68219287c18491b7aae8b3
MOZAN HAFIZ MAHARDHIKA <mozan.mahardhika@binus.ac.id>
2021-09-26 20:24:16 +0700

    Fixing admin-home style

--- Admin/home.php
+++ Admin/home.php
@@ -6,17 +6,51 @@
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/index.css">
 <body>
-    <ul class="breadcrumb">
-        <li>Dashboard</li>
-    </ul>
-    <hr>
+    <div class="page-view">
+        <ul class="breadcrumb">
+            <li>Dashboard</li>
+        </ul>
+        <hr>
 
-    <div class="page-header">
-        <h1>DASHBOARD</h1>
-    </div>
+        <div class="page-header">
+            <h1>DASHBOARD</h1>
+        </div>
 
-    <div class="page-note page-info">
-        <h3>Selamat datang di SiLab - Sistem Informasi Laboratorium</h3>
+        <div class="page-note page-info">
+            <h3>Selamat datang di E-Peminjaman - Peminjaman Elektronik Alat & Laboratorium</h3>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
     </div>
 </body>
 
