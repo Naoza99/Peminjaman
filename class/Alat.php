cd3548de37864a30b8891b2389a27efb705abf6e
Mozan <miceyud89@gmail.com>
2021-10-31 21:44:32 +0700

    Alat can be Deleted and Edited

--- class/Alat.php
+++ class/Alat.php
@@ -15,6 +15,56 @@
             $this->fm = new Format();
         }
 
+        public function ShowAlat(){
+            $query = "SELECT * 
+                        FROM Alat a
+                        JOIN AlatCategory ac ON ac.AlatCategoryId = a.ALatCategoryId
+                        JOIN Satuan s ON s.SatuanId = a.SatuanId
+                        JOIN Laboratorium l ON l.LaboratoriumId = a.LokasiSimpanId
+                        JOIN KondisiAlat ka ON ka.KondisiAlatId = a.KondisiAlatId";
+            $result = $this->db->select($query);
+            return $result;
+        }
+
+        public function ShowAlatById($id){
+            $query = "SELECT * FROM Alat WHERE AlatId = '$id'";
+            $result = $this->db->select($query);
+            return $result;
+        }
+
+        public function UpdateAlatById($data, $file, $id){
+            $AlatName = mysqli_real_escape_string($this->db->link, $data['AlatName']);
+            $AlatCategoryId = mysqli_real_escape_string($this->db->link, $data['AlatCategoryId']);
+            $Jumlah = mysqli_real_escape_string($this->db->link, $data['Jumlah']);
+            $Satuan = mysqli_real_escape_string($this->db->link, $data['SatuanId']);
+            // $datepicker = mysqli_real_escape_string($this->db->link, $data['datepicker']);
+            $LaboratoriumId = mysqli_real_escape_string($this->db->link, $data['LaboratoriumId']);
+            $KondisiAlat = mysqli_real_escape_string($this->db->link, $data['KondisiAlatId']);
+
+            $permited = array('jpg','png','jpeg','gif');
+            $file_name = $file['image']['name'];
+            $file_size = $file['image']['size'];
+            $file_temp = $file['image']['tmp_name'];
+
+            $div = explode('.', $file_name);
+            $file_ext = strtolower(end($div));
+            $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+            $uploaded_image = "upload/".$unique_image;
+
+            move_uploaded_file($file_temp, $uploaded_image);
+            $query = "UPDATE alat
+                        SET AlatName = '$AlatName', AlatCategoryId = '$AlatCategoryId', Jumlah = '$Jumlah', SatuanId = '$Satuan', LokasiSimpanId = '$LaboratoriumId', KondisiAlatId = '$KondisiAlat', Image = '$uploaded_image'
+                        WHERE AlatId = '$id'";
+            $result = $this->db->update($query);
+            if ($result) {
+    			$msg = "Product Updated Successfully.";
+    			return $msg;
+    		}else {
+    			$msg = "Product not Inserted";
+    			return $msg;
+    		} 
+        }
+
         public function ShowAllAlatCategory(){
             $query = "SELECT * FROM AlatCategory";
             $result = $this->db->select($query);
@@ -31,6 +81,19 @@
             $query = "SELECT * FROM KondisiALat";
             $result = $this->db->select($query);
             return $result;
+        }
+
+        public function delAlatById($id){
+            $query = "DELETE FROM Alat WHERE AlatId = '$id'";
+            $result = $this->db->delete($query);
+            if($result){
+                $msg = "Alat Deleted Successfully";
+                return $msg;
+            }
+            else{
+                $msg = "Alat Failed to be Deleted";
+                return $msg;
+            }
         }
 
         public function AlatInsert($data, $file){
